2a39aa68f291bfa0c88b236397653c83ee9f357d
Isaiah Thompkins <iThompkins@ithompkinss-air.devices.brown.edu>
2018-02-18 00:05:43 -0500

    Creating the development branch

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :wad
-  validates :content, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
-  get '/help',  to: 'static_pages#help'
-  get '/about',  to: 'static_pages#about'
-  get '/contact',  to: 'static_pages#contact'
-  get 'sessions/new'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'users/new'
-  get '/signup', to: 'users#new'
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
-  resources :wads do
-  	member do
-  		put 'like', to: 'wads#upvote'
-  	end
-  	resources :comments
-  		put 'like', to: 'comments#upvote'
-  end
+  get    '/industries', to: 'industries#index'
+  get    '/music',    to:  'industries#music'
+  get    '/tech',     to:  'industries#tech'
+  
 end
