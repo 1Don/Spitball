35e96acab7bff09755bae1120cb32483f2106348
1Don <nmorgan@wesleyan.edu>
2018-02-19 20:16:18 -0800

    Comment functionality added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219152801) do
+ActiveRecord::Schema.define(version: 20180220035435) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20180219152801) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "wads", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "short_form"
+    t.text "long_form"
+    t.text "category"
+    t.text "problem_state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_wads_on_category"
+    t.index ["user_id"], name: "index_wads_on_user_id"
   end
 
 end
