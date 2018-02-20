558742835df1293e760a52f9f53599cb44335d2d
1Don <nmorgan@wesleyan.edu>
2018-02-19 20:16:18 -0800

    Wads functionality added

---
+++ app/controllers/wads_controller.rb
@@ -0,0 +1,57 @@
+class WadsController < ApplicationController
+before_action :find_wad, only: [:show, :edit, :update, :destroy, :upvote]
+
+	def index
+		@wads = Wad.all.paginate(page: params[:page], per_page: 20)
+	end
+
+	def show
+		@wad = Wad.find(params[:id])
+	end
+
+	def new
+		@wad = Wad.new
+		end
+	
+
+	def create
+		@wad = current_user.wads.build(wad_params)
+		if @wad.save	
+			redirect_to @wad
+		else
+			flash[:error] = 'Error try again'
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @wad.update
+			redirect_to @wad
+		else
+			flash[]
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@wad.destroy
+		redirect_to root_path
+	end
+
+
+	private
+
+	def wad_params
+		params.require(:wad).permit(:problem_state, :short_form, :long_form, :category)
+	end	
+
+	def find_wad
+		@wad = Wad.where(params[:id])
+	end
+
+
+end
+
---
+++ app/models/wad.rb
@@ -0,0 +1,9 @@
+class Wad < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :category, presence: true
+  validates :long_form, presence: true, length: { maximum: 1000 }
+  validates :short_form, presence: true, length: { maximum: 140 }
+  validates :problem_state, presence: true, length: { maximum: 50 }
+end
