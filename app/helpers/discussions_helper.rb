14cf540846d591ea2d8fd01d039c9f455d392e09
Isaiah Thompkins <iThompkins@ithompkinss-air.devices.brown.edu>
2018-03-16 23:14:22 -0400

    Forum V2.1 - Fixed nesting

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,7 +1,7 @@
 module DiscussionsHelper
 	def discussions_tree_for(discussions)
-    discussions.map do |discussion, nested_discussions|
-      render(discussion) +
+    discussions.map do |discussions, nested_discussions|
+      render(discussions) +
           (nested_discussions.size > 0 ? content_tag(:div, discussions_tree_for(nested_discussions), class: "replies") : nil)
     end.join.html_safe
   end
