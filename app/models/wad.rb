a7afe3c43eabb1d0a0a9e8fb438d997adaf0d2e9
Isaiah Thompkins <iThompkins@ithompkinss-air.devices.brown.edu>
2018-03-17 21:31:04 -0400

    Wad V3

--- app/models/wad.rb
+++ app/models/wad.rb
@@ -1,4 +1,6 @@
 class Wad < ApplicationRecord
+  has_attached_file :image, styles: { medium: "250x250>", thumb: "50x50#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   acts_as_votable
   belongs_to :user
   has_many :comments, dependent: :destroy
