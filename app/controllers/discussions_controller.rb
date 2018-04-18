74bfa989f61264c98d6f8489da14d36846bb6259
Isaiah Thompkins <iThompkins@ithompkinss-air.devices.brown.edu>
2018-04-18 12:23:28 -0400

    Forum page restrictions

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,11 +2,17 @@ class DiscussionsController < ApplicationController
 	layout 'wad', only: [:index]
 
 	def index
-		@discussions = Discussion.all.paginate(page: params[:page], per_page: 20)
+		unless current_user != nil
+			@discussions = Discussion.all.paginate(page: params[:page], per_page: 20)
+		end
+		redirect_to root_path
 	end
 
 	def new
-		@discussion = Discussion.new
+		unless current_user != nil
+			@discussion = Discussion.new
+		end
+		redirect_to root_path
 	end
 
 	def destroy
