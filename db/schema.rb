3b066fc237fb1fad672bdc6b4f5c41559c90654b
Jose Ramon Camacho <joserracamacho@gmail.com>
2018-04-24 12:45:53 -0500

    Add responsive layouts

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20180422064619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
-    t.index ["user_id", "created_at"], name: "index_discussions_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
