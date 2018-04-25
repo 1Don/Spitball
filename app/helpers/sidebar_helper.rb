35f590ade8a8bb4e18936e36da1af1d4f850aeaa
Jose Ramon Camacho <joserracamacho@gmail.com>
2018-04-25 17:03:53 -0500

    Mobile fixes

---
+++ app/helpers/sidebar_helper.rb
@@ -0,0 +1,38 @@
+module SidebarHelper
+  def sidebar_elements
+    [
+      {
+        link: "/wads",
+        name: "All Wads"
+      },
+      {
+        link: "/art",
+        name: "Art"
+      },
+      {
+        link: "/b2bsaas",
+        name: "B2B Saas"
+      },
+      {
+        link: "/finance",
+        name: "Finance"
+      },
+      {
+        link: "/manufacturing",
+        name: "Manufacturing"
+      },
+      {
+        link: "/smallbusiness",
+        name: "Small Business"
+      },
+      {
+        link: "/consumertech",
+        name: "Consumer Tech"
+      },
+      {
+        link: "/other",
+        name: "Other"
+      }
+    ]
+  end
+end
