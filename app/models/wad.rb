7db3f87e6822e9eb3f2bed3402cca03a33d066eb
1Don <nmorgan@wesleyan.edu>
2018-05-04 18:58:53 -0700

    wad validations were mad short

--- app/models/wad.rb
+++ app/models/wad.rb
@@ -7,9 +7,9 @@ class Wad < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :category, presence: true
-  validates :long_form, presence: true, length: { maximum: 1000 }
-  validates :short_form, presence: true, length: { maximum: 140 }
-  validates :problem_state, presence: true, length: { maximum: 50 }
+  validates :long_form, presence: true, length: { maximum: 10000 }
+  validates :short_form, presence: true, length: { maximum: 1400 }
+  validates :problem_state, presence: true, length: { maximum: 500 }
 
 #Search Functionality
   def self.search(search)
