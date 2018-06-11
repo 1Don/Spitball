48b05b0769dbb52bb36ce6137576d4dc0cab8f74
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-06-11 02:36:32 -0400

    finished comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,4 +6,11 @@ module CommentsHelper
 				end
 	    end.join.html_safe
  	end
+
+	def reply_tree_for(replies, nested = false, id = nil)
+	    replies.map do |comment, nested_comments|
+				render(comment) +
+          (nested_comments.size > 0 ? content_tag(:div, reply_tree_for(nested_comments), class: "replies") : nil)
+	    end.join.html_safe
+ 	end 	
 end
