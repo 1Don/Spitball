e282d0c1fdd1925e8ff4925ce5938170a974f49e
1Don <nmorgan@wesleyan.edu>
2018-06-22 18:18:44 -0700

    fix remote header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180622034928) do
+ActiveRecord::Schema.define(version: 20180622031743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,15 +68,6 @@ ActiveRecord::Schema.define(version: 20180622034928) do
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friend_requests_on_friend_id"
     t.index ["user_id"], name: "index_friend_requests_on_user_id"
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -152,6 +143,5 @@ ActiveRecord::Schema.define(version: 20180622034928) do
 
   add_foreign_key "discussions", "users"
   add_foreign_key "friend_requests", "users"
-  add_foreign_key "friendships", "users"
   add_foreign_key "wads", "users"
 end
