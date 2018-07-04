72651befc71c2c6035e1b5f9cccfb9e67cd64b3b
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-07-03 21:50:49 -0400

    made posting happen real time on message index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -36,9 +36,7 @@ class MessagesController < ApplicationController
 
 	def create
 		 @message = @conversation.messages.new(message_params)
-		 if @message.save
-		  redirect_to conversation_messages_path(@conversation)
-		 end
+		 @message.save
 	end
 
 
