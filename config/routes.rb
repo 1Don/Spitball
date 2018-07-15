70a7faa96b995fbb0a190aeb46057ce3bf385008
softcomet <realseniordev210@gmail.com>
2018-07-15 10:44:44 +0200

    add assets and modify template

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,43 @@
 Rails.application.routes.draw do
 
-  resources :friend_requests
-  get 'friendships/create'
-
-  get 'friendships/destroy'
-
-  get 'sessions/new'
-
-  get 'users/new'
-
   root   'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+
+  resources :friend_requests
+
+  get 'friendships/create'
+  get 'friendships/destroy'
+  get 'sessions/new'
+  get 'users/new'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get    '/comingsoon',  to: 'static_pages#landing'
-  get    '/popular',  to:'wads#popwads'
-  
+  get '/comingsoon',  to: 'static_pages#landing'
+  get '/popular',  to:'wads#popwads'
+  get '/tech',    to:  'wads#consumertech'
+  get '/b2b',     to:  'wads#b2b'
+  get '/product',     to:  'wads#product'
+  get '/social',     to:  'wads#social'
+  get '/local',     to:  'wads#local'
+  get '/events',     to:  'wads#events'
+  get '/innovate',     to:  'wads#innovate'
+  get '/media',     to:  'wads#media'
+  get '/forum',    to:   'discussions#index'
 
-  get    '/tech',    to:  'wads#consumertech'
-  get    '/b2b',     to:  'wads#b2b'
-  get    '/product',     to:  'wads#product'
-  get    '/social',     to:  'wads#social'
-  get    '/local',     to:  'wads#local'
-  get    '/events',     to:  'wads#events'
-  get    '/innovate',     to:  'wads#innovate'
-  get    '/media',     to:  'wads#media'
+  #Defining routes for user profile navigation
+  get '/users/:id/profile_comments', to: 'users#profile_comments', as: :profile_comments
+  get '/users/:id/profile_mail', to: 'users#profile_mail', as: :profile_mail
 
-  get    '/forum',    to:   'discussions#index'
-
-
-
-#Defining routes for user profile navigation
-    get    '/users/:id/profile_comments', to: 'users#profile_comments', as: :profile_comments
-    get    '/users/:id/profile_mail', to: 'users#profile_mail', as: :profile_mail
-
-#Defining routes for search navigation
-    get    '/search', to: 'static_pages#search'
+  #Defining routes for search navigation
+  get '/search', to: 'static_pages#search'
 
   resources :conversations do
     resources :messages
-   end
+  end
 
- 
   resources :notifications do
     collection do
       post :mark_as_read
@@ -63,7 +55,6 @@ Rails.application.routes.draw do
         put "like", to: "answers#upvote"
       end   
   end
-
 
   resources :wads do
     member do
