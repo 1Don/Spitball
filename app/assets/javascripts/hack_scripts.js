1c403ffece6f05ea1daaa0853a6410a669491e49
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-07-24 18:52:00 -0400

    Made page scroll to what we do section

--- app/assets/javascripts/hack_scripts.js
+++ app/assets/javascripts/hack_scripts.js
@@ -3,3 +3,11 @@ function sendPost() {
 	$.post('/notifications/mark_as_read')
 }
 
+//Hacks for landing page scroll
+$(document).ready(function() {
+	$("#landing-btn").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#what-we-do").offset().top},
+	        'slow');
+	});	
+})
