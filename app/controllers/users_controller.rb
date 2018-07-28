436daa14b9993e68f6dd8909bad9ff8a974be062
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-07-27 20:07:10 -0400

    Complete mailer functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.signup_confirmation(@user).deliver
       log_in @user
       flash[:success] = "Welcome to Our Beta!"
       redirect_to @user
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+  #
+  def signup_confirmation(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
