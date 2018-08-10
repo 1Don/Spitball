2c48c71b0f996411ce10ff43801344f3fe55b487
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-08-10 09:49:24 -0400

    a little responsiveness done. just a test

--- app/assets/javascripts/hack_scripts.js
+++ app/assets/javascripts/hack_scripts.js
@@ -7,13 +7,36 @@ function sendPost() {
 
 $(document).on('turbolinks:load', function() {
 	$('#wad-new-form').hide();
+	if ($(window).width() <= 570 ){
+		$('#nav-menu').hide();
+	}
 	$('#menu-btn').click(function(){
-		$('#wad-new-form').hide();
-		$('#nav-menu').show()
+		if ($('#nav-menu').is(":hidden")){
+			$('#wad-new-form').hide();
+			$('#nav-menu').show()
+		} else {
+			if ($(window).width() > 600 ){
+				$('#nav-menu').show()
+				$('#wad-new-form').hide()	
+			} else {
+				$('#wad-new-form').hide()
+				$('#nav-menu').hide()
+			}
+		}
 	});
 	$('#new-wad-btn').click(function(){
-		$('#nav-menu').hide();
-		$('#wad-new-form').show()
+		if ($('#wad-new-form').is(":hidden")){	
+			$('#nav-menu').hide();
+			$('#wad-new-form').show()
+		} else {
+			if ($(window).width() > 600 ){
+				$('#nav-menu').hide();
+				$('#wad-new-form').show()
+			} else {
+			$('#wad-new-form').hide()
+			$('#nav-menu').hide()
+			}
+		}
 	});	
 	const orig = $('#comment-tab-open').html()
 	$('#comments-display').hide()
