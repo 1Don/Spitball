7d7f01a2235481e7d27ce4ae97cd1fd63bce6b1a
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-08-12 12:44:16 -0400

    Fixed sign up mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
 	default from: 'no-reply@spitball.io'
 	
-	def welcome_email
-	    @user = params[:user]
-	    @url  = 'http://spitball.io/login'
-	    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+	def welcome_email(user)
+		if user
+			@user = user
+		    @url  = 'http://spitball.io/login'
+		    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+		else
+		    @user = params[:user]
+		    @url  = 'http://spitball.io/login'
+		    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+		end
  	end
 end
