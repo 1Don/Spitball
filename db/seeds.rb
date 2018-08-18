375a141bedd0521d6e008de148ae908d45b79fa0
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2018-08-17 23:46:40 -0400

    tinkering

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,17 @@ User.create!([{name: 'iThompkins',
 			   password: 'foobar', 
 			   password_confirmation: 'foobar', 
 			   first_name:'Isaiah', 
-			   last_name: 'Thompkins'},
+			   last_name: 'Thompkins',
+			   photo_file_name: nil, 
+			   photo_content_type: nil, 
+			   photo_file_size: nil},
 
 			   {name: 'Nick Morgan', 
 			   email: 'nickmorgansc@gmail.com', 
 			   password: 'foobar', 
 			   password_confirmation: 'foobar', 
 			   first_name:'Nick', 
-			   last_name: 'Morgan'}])
+			   last_name: 'Morgan'			   
+			   photo_file_name: nil, 
+			   photo_content_type: nil, 
+			   photo_file_size: nil}])
