1bc443e98f6ac546b6addce05eade6385de971c1
Isaiah Thompkins <iThompkins@ithompkinss-air.devices.brown.edu>
2018-10-07 13:52:30 -0400

    pulled comment votes so they're visible

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,8 @@
 module CommentsHelper
 	def comments_tree_for(replies, nested = false, id = nil)
 	    replies.map do |comment, nested_comments|
-				content_tag(:div, id: id, class: "comment mb-1 border-bottom") do
-					render(comment) 
+				content_tag(:div, id: id, class: "comment border-bottom") do
+					render(comment)
 				end
 	    end.join.html_safe
  	end
@@ -12,5 +12,5 @@ module CommentsHelper
 				render(comment) +
           (nested_comments.size > 0 ? content_tag(:div, reply_tree_for(nested_comments), class: "replies") : nil)
 	    end.join.html_safe
- 	end 	
+ 	end
 end
