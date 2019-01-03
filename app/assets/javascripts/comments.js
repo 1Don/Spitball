5ac9ee3c58f14901a52ba59cff59bcc57c17774a
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2019-01-02 22:57:14 -0500

    comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
   $('.options').hide()
-  //$('.reply-hide-area').hide()
-  //$('.comment-reply-btn').hide()
+  $('.reply-hide-area').hide()
+  $('.cmt-forms').hide()
 
-  $('.comment-reply-form').focus(function(){
-    $('.comment-reply-btn').show()
+  $('.cancel-btn').click(function(){
+    $('.form-field').val("");
+    $('.comment-reply-btn').css("opacity", "0.5");
+    $('.cancel-btn').hide()
+    $('.hid-field').val("");
   })
-
 })
+function activeBox(id, big = false){
+  if (big){
+    if ($('#parent-cmt-field').val().length > 0){
+      $('#parent-cmt-btn').css("opacity", "1")
+    } else {
+      $('#parent-cmt-btn').css("opacity", "0.0")
+    }
+  }else{
+    if ($(".form-field-" + id).val().length > 0){
+      $('.comment-reply-btn').css("opacity", "1")
+      $('.cancel-btn').show()
+    } else {
+      $('.comment-reply-btn').css("opacity", "0.5")
+      $('.cancel-btn').hide()
+    }
+  }
+}
--- app/assets/javascripts/iconInteractions.js
+++ app/assets/javascripts/iconInteractions.js
@@ -1,33 +1,40 @@
+
 //Runs all setters
+function seeReply(id){
+	if ($('#reply-' + id).is(":hidden")){
+		$('.reply-hide-area').slideUp()
+		$('.see-replies').html('Show replies <i class="fa fa-caret-down"></i>')
+		$('#see-replies-' + id).html('Close replies <i class="fa fa-caret-up"></i>')
+		$('#reply-' + id).slideToggle()
+	} else {
+		$('#see-replies-' + id).html('Show replies <i class="fa fa-caret-down"></i>')
+		$('#reply-' + id).slideUp()
+	}
+}
+
 function execReply(id){
+	setVal(id);
+	setFormId(id);
+	$('.cancel-btn').hide()
+	$('.form-field').val(null);
+	$('.comment-reply-btn').css("opacity", "0.5")
+	if ($("#cmt-form-" + id).is(":hidden")){
+		$(".cmt-forms").hide();
+		$("#cmt-form-" + id).slideToggle()
+	}else{
+		$("#cmt-form-" + id).slideToggle();
+	}
+}
 
-		$('.form-field').val(null);
-		setVal(id);
-		setFormId(id);
-		toggleReply(id);
-
-		//Toggles reply tree on and off
-		function toggleReply(id) {
-			if ($('#reply-' + id).is(":hidden")){
-				$('.reply-hide-area').slideUp()
-				$('.see-replies').html('Show replies <i class="fa fa-caret-down"></i>')
-				$('#see-replies-' + id).html('Close replies <i class="fa fa-caret-up"></i>')
-				$('#reply-' + id).slideToggle()
-			} else {
-				$('#see-replies-' + id).html('Show replies <i class="fa fa-caret-down"></i>')
-				$('#reply-' + id).slideUp()
-			}
-		}
-
+function secondaryReply(cmt_id, id, name) {
+	execReply(id);
+	$('.comment-reply-btn').css("opacity", "1")
+	setVal(id);
+	setReply(cmt_id, name);
 }
 
 function focusForm(id){
 	$("#comment-reply-box-" + id).focus()
-}
-
-function secondaryReply(cmt_id, id, name) {
-	setVal(id);
-	setReply(cmt_id, name);
 }
 
 //Gives each parent_id hidden field its ID
