4e419ebd73befdc4b4666c4448c748a1aa56e3e9
1Don <nmorgan@wesleyan.edu>
2019-02-02 18:53:36 -0800

    refresh key to migrate from g+ to google sign in

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :google_oauth2,
     '289714810643-cng8l1klcojnclpvmamnhv7mgb3qre39.apps.googleusercontent.com',
-    '39PPXfT4MYeta9lPSMnv2qo6',
+    '5iZFw57OnBOmLIJZDhzfIHIX',
     callback_path: "/auth/google_oauth2/callback"
   )
 
